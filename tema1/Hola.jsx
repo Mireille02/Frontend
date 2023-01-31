80ab2b37684a33bd115993c22161131b5cd92422
Mireille Caballero Ramírez <Mireille>
2023-01-30 22:14:10 -0600

    Módulo 4, Sesión 1

---
+++ tema1/Hola.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native/types";
+
+const HolaComponent = () => {
+    return (
+        <Text>Hola</Text>
+    )
+}
+
+export default HolaComponent
