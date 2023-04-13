7f0bf4c40b20557122bf785d1e1452267f766546
Mireille <mire.flor4@gmail.com>
2023-04-12 21:30:50 -0600

    Módulo 5, Clase 8

---
+++ modulo5/Sesion7/consumo.js
@@ -0,0 +1,13 @@
+const API_ENDPOINT = "https://api.ipbase.com/v1/json/";
+
+fetch(API_ENDPOINT)
+.then(response => response.json())
+.then(datosUbicacion => {
+  // Imprimir los datos de la ubicación
+  console.log(datosUbicacion);
+  // Recuerda que podemos acceder a latitude y longitude, entre otros
+  const latitud = datosUbicacion.latitude,
+    longitud = datosUbicacion.longitude;
+    
+  console.log(`Tus coordenadas son ${latitud},${longitud}`);
+});git
